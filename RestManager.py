c24184ea50c0c2d02095a4b7ea6cbd329a57e60f
sp.M2GLa <Na@na.na>
2017-04-26 15:38:01 +0200

    add post methode

--- RestManager.py
+++ RestManager.py
@@ -14,3 +14,11 @@ def get_drone(id_intervention):
 
 def post_photo(value):
     requests.post(path+'photo',data = value)
+
+def post_position(position, id_intervention):
+    value = {"position": [position[0],position[1]], "idintervention": id_intervention}
+    requests.post(path+'positiondrone',data = value)
+
+def post_photo(position, dateheure, nom, path, positionPTS, id_intervention):
+    value = {"position": [position[0],position[1]], "date_heure": dateheure, "nom": nom, "path": path, "positionPTS": [positionPTS[0],positionPTS[1]], "idintervention": id_intervention}
+    requests.post(path + 'photo', data=value)
