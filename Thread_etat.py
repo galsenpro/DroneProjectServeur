512d7d549f87d6eb6c9c06383b33aef5db1fc26b
sp.M2GLa <Na@na.na>
2017-04-27 09:23:40 +0200

    test tread ok

--- Thread_etat.py
+++ Thread_etat.py
@@ -1,11 +1,15 @@
+import os
 from threading import Thread,Event
+
+import sys
+
 from Drone import Drone
 #from ParcoursSegment import ParcoursSegment
 import RestManager as RM
 import time
 
 class Thread_etat(Thread):
-    def __init__(self,drone,refresh = 5.0):
+    def __init__(self,drone = None,refresh = 1.0):
         super(Thread_etat, self).__init__()
         self.drone = drone
         self._stopevent = Event()
@@ -14,32 +18,56 @@ class Thread_etat(Thread):
         self.ps = None
 
     def run(self):
-        id_inter = self.drone.id_intervention
+        # id_inter = self.drone.id_intervention
         while not self._stopevent.isSet():
-            res = RM.get_drone(id_inter)
-            new_etat = res['etat']
-            print(new_etat)
-            """if new_etat != self.etat:
-                self.etat = new_etat
-                if self.etat == 'SEGMENT':
-                    parcours = res['segment']
-                    self.ps = ParcoursSegment(self.drone,parcours.points,parcours.boucle_fermee)
-                    self.ps.parcourir_segments()
-                if self.etat == 'STOP':
-                    self.ps.stop_parcours()
-                if self.etat == 'ZONE':
-                    pass"""
+            print "Hello !"
+            # res = RM.get_drone(id_inter)
+            # new_etat = res['etat']
+            # print(new_etat)
+            # """if new_etat != self.etat:
+            #     self.etat = new_etat
+            #     if self.etat == 'SEGMENT':
+            #         parcours = res['segment']
+            #         self.ps = ParcoursSegment(self.drone,parcours.points,parcours.boucle_fermee)
+            #         self.ps.parcourir_segments()
+            #     if self.etat == 'STOP':
+            #         self.ps.stop_parcours()
+            #     if self.etat == 'ZONE':
+            #         pass"""
             self._stopevent.wait(self.refresh)
         print('fin du thread')
 
     def stop(self):
         self._stopevent.set()
 
-drone = Drone()
-drone.set_intervention('58ddf84c212566155e8e98ec')
-print('drone OK, début lecture commandes')
-tetat = Thread_etat(drone)
-tetat.start()
-time.sleep(30)
-print('fin du script')
-tetat.stop()
+
+# drone = Drone()
+# drone.set_intervention('58ddf84c212566155e8e98ec')
+# print('drone OK, début lecture commandes')
+# tetat = Thread_etat(drone)
+# tetat.start()
+# time.sleep(30)
+# print('fin du script')
+# tetat.stop()
+
+def main():
+    # drone = Drone()
+    # drone.set_intervention('58ddf84c212566155e8e98ec')
+    # print('drone OK, debut lecture commandes')
+    tetat = Thread_etat()
+
+    print('script start')
+    tetat.start()
+    time.sleep(10)
+    print('script stop')
+    tetat.stop()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print 'Interrupted'
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
