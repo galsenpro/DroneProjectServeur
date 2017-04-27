9ae424c2ed7b310eae77ce31119d47b0e4c7e60d
KimAndGumi <nirina.ranaivoson@gmail.com>
2017-04-27 15:50:45 +0200

    thread_video

--- Thread_video.py
+++ Thread_video.py
@@ -1,12 +1,5 @@
-import os
 from threading import Thread,Event
 
-import sys
-
-from Drone import Drone
-#from ParcoursSegment import ParcoursSegment
-import RestManager as RM
-import time
 
 class Thread_position(Thread):
     def __init__(self,drone = None,refresh = 1.0):
@@ -14,20 +7,18 @@ class Thread_position(Thread):
         self.drone = drone
         self._stopevent = Event()
         self.refresh = refresh
-        self.etat = None
-        self.ps = None
 
     def run(self):
         id_inter = self.drone.id_intervention
         while not self._stopevent.isSet():
-            print self.drone.getGPSCoordonate()
+            self.drone.prend_video()
             self._stopevent.wait(self.refresh)
         print('fin du thread')
 
     def stop(self):
         self._stopevent.set()
 
-def main():
+"""def main():
     drone = Drone()
     drone.set_intervention('58ddf84c212566155e8e98ec')
     print('drone OK, debut lecture commandes')
@@ -46,4 +37,4 @@ if __name__ == '__main__':
         try:
             sys.exit(0)
         except SystemExit:
-            os._exit(0)
+            os._exit(0)"""
