3bd21294483a89063fa64f1bf5368675eb05529b
sp.M2GLa <Na@na.na>
2017-04-28 10:51:31 +0200

    ajout d'un drone a une intervention for test

--- AjoutDroneIntervention.py
+++ AjoutDroneIntervention.py
@@ -5,12 +5,11 @@
 import RestManager as RM
 from dronekit import LocationGlobalRelative
 from Drone import Drone
-from ParcoursSegment import ParcoursSegment
 
 #recup des différentes interventions
 from Thread_position import Thread_position
 
-#print(RM.get_interventions())
+print(RM.get_interventions())
 
 print("Récupérations des interventions")
 #affichage de numéro - nom intervention
@@ -33,14 +32,14 @@ for intervention in interventions:
     i=i+1
 
 var = raw_input("\n\r\nNuméro de l'intervention à sélectionner: ")
-#print "intervention n°", var
+print "intervention n°", var
 
-#print interventions[int(var)]["_id"] # l'id de l'intervention
-#print interventions[int(var)]
+print interventions[int(var)]["_id"] # l'id de l'intervention
+print interventions[int(var)]
 
 idIntervention = interventions[int(var)]["_id"]
 position = interventions[int(var)]["position"]
-#print position
+print position
 
 #Start SITL if no connection string specified
 sitl = None
@@ -48,7 +47,7 @@ import dronekit_sitl
 #démarage du tread SITL avec comme position
 sitl = dronekit_sitl.start_default(position[0],position[1]+0.0005)
 connection_string = sitl.connection_string()
-#print  connection_string
+print  connection_string
 
 # Connect to the Vehicle
 print 'Connecting to vehicle on: %s' % connection_string
@@ -60,30 +59,6 @@ drone = Drone(connection_string, id_intervention=idIntervention)
 tetat = Thread_position(drone)
 print('script start')
 tetat.start()
-
-drone_dernierEtat = "STOP"
-#thread de parcours (Segment ou Zone)
-ps = None
-while True:
-    resDrone = RM.get_drone(drone.id_intervention)
-    drone.set_etat(resDrone['etat'])
-    if drone_dernierEtat != drone.etat:
-        drone_dernierEtat = drone.etat
-        if drone.etat == 'SEGMENT':
-            parcours = resDrone['segment']
-            ps = ParcoursSegment(drone,parcours['points'],parcours['boucleFermee'])
-            ps.start()
-        if drone.etat == 'ZONE':
-            pass
-            # demarer un thread pour la zone
-        if drone.etat == 'STOP':
-            if ps:
-                ps.stop()
-                print('fin de parcours')
-            #drone.aller_a(drone.getGPSCoordonate())
-            # stopper les thread zone et segment
-        if drone.etat == 'PARKING':
-            drone.RTLandFinish()
 
 goto1 = LocationGlobalRelative(position[0], position[1]+0.0005, 20)
 goto2 = LocationGlobalRelative(position[0]+0.0006, position[1]-0.0010, 20)
