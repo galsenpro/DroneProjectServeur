13082bf488b3571169d8cf402c7ded7da5e4d39b
sp.M2GLa <Na@na.na>
2017-05-03 10:55:34 +0200

    add zone

--- ParcoursZoneRandom.py
+++ ParcoursZoneRandom.py
@@ -1,6 +1,8 @@
-import dronekit_sitl
-from dronekit import Vehicle,LocationGlobalRelative,Command
-from pymavlink.mavutil import mavlink
+import random
+
+import sys
+from dronekit import LocationGlobalRelative
+from threading import Thread,Event
 
 from Drone import Drone
 
@@ -12,49 +14,69 @@ class DroneZoneRandom():
 
     def __init__(self,contour=None,d = 10):
         self.contour = contour
+        self.listeTotal = []
+        self._stopevent = Event()
         self.d = d
 
-    def convertionArrayLonLat(contours):
+    def convertionArrayLonLat(self,contours):
         liste = None
         for point in contours:
             liste.append(LocationGlobalRelative(point[0],point[1]))
         return liste
 
-    def parcourir_zone(self,drone):
-        listeinit = self.convertionArrayLonLat(self.contour)
-        listeTotal = None
+    def calculPointsIntermediaires(self, pts1, pts2):
+        print "from "+str(pts1.lat)+" "+str(pts1.lon)+"   to   "+str(pts2.lat)+" "+str(pts2.lon)
+        vec = LocationGlobalRelative(0, 1)
+        vec.lat = pts2.lat - pts1.lat
+        vec.lon = pts2.lon - pts1.lon
+        for nb in range(1, 10, 1):
+            npts = LocationGlobalRelative(pts1.lat+vec.lat*nb/10.0,pts1.lon+vec.lon*nb/10.0)
+            print " "+str(npts.lat)+" "+str(npts.lon)
+            self.listeTotal.append(npts)
 
-        for i in range(listeinit):
-            if i == listeinit.size -1:
+    def parcourir_zone(self,drone):
+        # listeinit = self.convertionArrayLonLat(self.contour)
+        listeinit = self.contour
+
+        for i in range(len(listeinit)):
+            if i == len(listeinit) -1:
+                print "1"+str(i)
+                self.calculPointsIntermediaires(listeinit[0],listeinit[len(listeinit)-1])
                 None#calculer les points intermediaires entre le dernier et le 1er
             else:
-
+                print "2"+str(i)
+                self.calculPointsIntermediaires(listeinit[i], listeinit[i+1])
                 None# calculer la liste des points intermediaire listeinit[i] listeinit[i+1]
 
-        while True:
-            randomNumber = None
-            #(generaterandom * listeTotal.size)%listeTotal.size/2
-            drone.goTo(listeTotal[randomNumber])
-            self.attendre_parcours(drone,listeTotal[randomNumber])
+        position = 0
+        while not self._stopevent.isSet():
+            randomNumber = random.randint(0,len(self.listeTotal)-1)/2
+            position = ((position + (len(self.listeTotal)/4))+randomNumber)%len(self.listeTotal)
+            print "Position "+str(position)
+
+            drone.goTo(self.listeTotal[position])
+            self.attendre_parcours(drone,self.listeTotal[position])
 
 
 
-
-
-
-points = []
-points.append(LocationGlobalRelative(-5,5))
-points.append(LocationGlobalRelative(-5,-6))
-points.append(LocationGlobalRelative(-10,-8))
-
-sitl = dronekit_sitl.start_default(0,0)
-connection_string = sitl.connection_string()
-#print  connection_string
-
-# Connect to the Vehicle
-print 'Connecting to vehicle on: %s' % connection_string
-
-dzr = DroneZoneRandom(points)
-drone = Drone(connection_string)
-drone.setAltitude(20)
-dzr.parcourir_zone()
+    def stop(self):
+            self._stopevent.set()
+            sys.exit(0)
+pointss = []
+pointss.append(LocationGlobalRelative(5.0,5.0))
+pointss.append(LocationGlobalRelative(6.0,4.0))
+pointss.append(LocationGlobalRelative(6.0,6.0))
+# sitl = None
+# import dronekit_sitl
+# sitl = dronekit_sitl.start_default(lat=10.0,lon=20.0)
+# connection_string = sitl.connection_string()
+# #print  connection_string
+#
+# # Connect to the Vehicle
+# print 'Connecting to vehicle on: %s' % connection_string
+#
+# drone = Drone(connection_string,20,id_intervention="ididi")
+# drone.arm_and_takeoff(20)
+# drone.aller_a(LocationGlobalRelative(20, 21+0.0005), 20)
+dzr = DroneZoneRandom(pointss)
+dzr.parcourir_zone(None)
